23199f6693fa05d81a6d2c48634c46f7d8cb8af7
EddySamuilov <samuilow10e@gmail.com>
2024-11-30 20:47:05 +0200

    Add price and imageurl to the product model

--- src/main/java/com/example/products/dtos/ProductSearchResponseDTO.java
+++ src/main/java/com/example/products/dtos/ProductSearchResponseDTO.java
@@ -4,14 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductSearchResponseDTO {
-
   private String title;
-
   private String description;
-
   private String type;
+  private String imageUrl;
+  private BigDecimal price;
+  private LocalDateTime created;
+  private LocalDateTime modified;
 }
--- src/main/java/com/example/products/models/Product.java
+++ src/main/java/com/example/products/models/Product.java
@@ -13,6 +13,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @Entity
 @Table(name = "products")
@@ -26,6 +28,11 @@ public class Product extends BaseEntity {
   @Column(nullable = false)
   @Size(min = 5, max = 255, message = "The description must be at least 5 characters and must not exceed 255 characters!")
   private String description;
+
+  private String imageUrl;
+
+  @Column(nullable = false)
+  private BigDecimal price;
 
   @Enumerated(EnumType.STRING)
   private ProductType type;
